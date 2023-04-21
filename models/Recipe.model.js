476b728fc7cc610631e296f0d44e2fc63046bcec
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-04-21 17:05:41 -0400

    changes to iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,19 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-  title: String,
-  level: {enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  title: { type: String, required: true, unique: true },
+  level: { enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"] },
   ingredients: [String],
-  cuisine: {type: String, required: true},
-  dishType: {enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']},
-  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
-  duration: {type: Number, min: 0},
+  cuisine: { type: String, required: true },
+  dishType: {
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: { type: Number, min: 0 },
   creator: String,
-  created: {type: Date, default: Date.now}
+  created: { type: Date, default: Date.now },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
